e0b8459dc79140fac2346ef8fe00a2b04d73fcb6
azeem-io <azeemswr.as@gmail.com>
2025-03-02 12:06:42 +0500

    Intial commit

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+
+export let seo = {
+	title: 'Cache Logic',
+	description:
+		'Empowering businesses with cutting-edge technology and tailored solutions.',
+	image: 'https://i.postimg.cc/L4ypsV5s/Group-2.png',
+	twitter: 'SEO twitter',
+	url: 'https://cache-logic.vercel.app',
+	keywords: 'cache logic, IT service'
+};
+
