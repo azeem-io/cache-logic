1c6fe82acab229fe0121e87cb8c4b133adf4ab62
azeem-io <azeemswr.as@gmail.com>
2025-03-04 00:02:49 +0500

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+	<h1 class="text-balance text-center text-9xl font-bold text-red-600">
+		{page.status}
+	</h1>
+	<p class="text-5xl font-semibold">{page.error?.message ?? 'Unknow Error Occured'}</p>
+	<Button
+		on:click={() => {
+			goto('/');
+		}}
+		class="mt-6">Back To Home</Button
+	>
+</div>
